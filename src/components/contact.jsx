32cc7e10e22f2e711e0da6fa7265510d6525ef70
JavadAlip <javadalipp911@gmail.com>
2024-12-14 12:00:23 +0530

    Components code cleanup

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 const Contact = () => {
     const emailAddress = 'javadalipp911@gmail.com';
     return (
@@ -12,8 +10,7 @@ const Contact = () => {
                     </p>
                     <p className='mt-2 text-white gradient-text-animation font-space text-[18px]' >Get in touch to collaborate</p>
                     <p className='text-2xl text-center border rounded p-4 mt-6 relative' style={{ borderRadius: '18px' }}>
-                        {/* <FontAwesomeIcon icon={faEnvelope} style={{ marginRight: '8px' ,  width: '18px', height: '18px', }} /> */}
-                        <a href={`mailto:${emailAddress}`} className=' font-space gradient-text-animation text-[18px] hover:text-gray-400'style={{ fontSize: 'smaller' }}>
+                        <a href={`mailto:${emailAddress}`} className=' font-space gradient-text-animation text-[18px] hover:text-gray-400' style={{ fontSize: 'smaller' }}>
                             {emailAddress}
                         </a>
                     </p>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,101 +1,3 @@
-// import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
-// const Home = () => {
-//   return (
-//     <div name='home' className='w-full min-h-screen flex items-center justify-center'>
-//       <div className='max-w-[1000px] w-full px-8 flex flex-col justify-center h-full'>
-//         <div className='mt-auto md:mt-0'>
-//           <p className='text-4xl text-gray-400'>Hi, I am</p>
-//           <h1 className='font-bold text-6xl text-gray-400'>Javad Ali</h1>
-//           <h2 className='font-bold text-6xl mb-4'>MERN Stack Developer</h2>
-//           <p className='mb-4 max-w-[725px]'>
-//           A self-taught full-stack developer focused on delivering robust and high-quality solutions, committed to innovation and technology. Enthusiastic about building impactful applications.
-//           </p>
-//           <div className='flex'>
-//             <a href="https://www.linkedin.com/in/javadalip/" target="_blank" rel="noopener noreferrer" className="mr-4 hover:text-gray-400">
-//               <FontAwesomeIcon icon={faLinkedin} size="2x" />
-//             </a>
-//             <a href="https://github.com/JavadAlip" target="_blank" rel="noopener noreferrer" className="mr-4 hover:text-gray-400">
-//               <FontAwesomeIcon icon={faGithub} size="2x" />
-//             </a>
-//             <a href="https://www.instagram.com/javaaad.ali/" target="_blank" rel="noopener noreferrer" className="mr-4 hover:text-gray-400">
-//               <FontAwesomeIcon icon={faInstagram} size="2x" />
-//             </a>
-//             {/* <a href="https://twitter.com/JavadALIp" target="_blank" rel="noopener noreferrer" className="mr-4 hover:text-gray-400">
-//               <FontAwesomeIcon icon={faXTwitter} size="2x" />
-//             </a> */}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Home;
-
-
-
-
-// import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
-// const Home = () => {
-//   return (
-//     <div
-//       name="home"
-//       className="w-full min-h-screen flex items-center justify-center bg-black"
-//     >
-//       <div className="max-w-[1000px] w-full px-8 flex flex-col justify-center h-full relative z-10">
-//         <div className="mt-auto md:mt-0">
-//           <p className="text-4xl text-gray-400">Hi, I am</p>
-//           <h1 className="font-bold text-6xl gradient-text-animation">
-//             Javad Ali
-//           </h1>
-//           <h2 className="font-bold text-6xl mb-4 gradient-text-animation">
-//             MERN Stack Developer
-//           </h2>
-//           <p className="mb-4 max-w-[725px] text-gray-400">
-//             A self-taught full-stack developer focused on delivering robust and
-//             high-quality solutions, committed to innovation and technology.
-//             Enthusiastic about building impactful applications.
-//           </p>
-//           <div className="flex">
-//             <a
-//               href="https://www.linkedin.com/in/javadalip/"
-//               target="_blank"
-//               rel="noopener noreferrer"
-//               className="mr-4 hover:text-gray-400 text-white"
-//             >
-//               <FontAwesomeIcon icon={faLinkedin} size="2x" />
-//             </a>
-//             <a
-//               href="https://github.com/JavadAlip"
-//               target="_blank"
-//               rel="noopener noreferrer"
-//               className="mr-4 hover:text-gray-400 text-white"
-//             >
-//               <FontAwesomeIcon icon={faGithub} size="2x" />
-//             </a>
-//             <a
-//               href="https://www.instagram.com/javaaad.ali/"
-//               target="_blank"
-//               rel="noopener noreferrer"
-//               className="mr-4 hover:text-gray-400 text-white"
-//             >
-//               <FontAwesomeIcon icon={faInstagram} size="2x" />
-//             </a>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
@@ -127,7 +29,7 @@ const Home = () => {
               rel="noopener noreferrer"
               className="mr-4 gradient-icon  text-white hover:opacity-75"
             >
-              <FontAwesomeIcon icon={faLinkedin} size="2x" /> 
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
             </a>
             <a
               href="https://github.com/JavadAlip"
