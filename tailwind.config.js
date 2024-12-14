26c5deed467e2d359522bd925688b9c7a6ab5492
JavadAlip <javadalipp911@gmail.com>
2024-12-14 11:33:53 +0530

    last one inlcudeing add some new features

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        space: ['Space Mono', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
